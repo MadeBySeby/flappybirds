f919814e51a7d9ed5b74a2c6a42309ec346e8af6
dato sebiskveradze <david.katsitadze.1@btu.edu.ge>
2025-06-07 14:52:30 +0400

    your update message

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { Application, Assets, Sprite } from "pixi.js";
+
+(async () => {
+  // Create a new application
+  const app = new Application();
+
+  // Initialize the application
+  await app.init({ background: "#1099bb", resizeTo: window });
+
+  // Append the application canvas to the document body
+  document.getElementById("pixi-container").appendChild(app.canvas);
+
+  console.log("s");
+})();
